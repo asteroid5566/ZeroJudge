7b282acb1b2bf781e481b7f0184822f440b7c8e5
asteroid5566 <47234920+asteroid5566@users.noreply.github.com>
2021-02-04 16:04:52 +0800

    solution

---
+++ a022-迴文.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string str;
+	bool flag;
+	
+	while (cin >> str){
+		flag = true;
+		
+		for (int i = 0; i < str.length() / 2; i++){
+			if (str[i] != str[str.length() - 1 - i]){
+				flag = false;
+				break;
+			}	
+		}
+		flag ? cout << "yes" << endl : cout << "no" << endl;
+	}
+	
+	return 0;
+}
