753aa50ee29b05f3aa3c6628f5a003d14a915dcb
asteroid5566 <47234920+asteroid5566@users.noreply.github.com>
2021-02-08 18:44:17 +0800

    solution

---
+++ a038-數字翻轉.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string str;
+	cin >> str;
+	bool showZero = false, noneZero = false;
+	
+	for (int i = str.length() - 1; i >= 0; i--){
+		if (str[i] == '0' && showZero == false){
+			if (str[i - 1] != '0'){
+				showZero = true;
+			}
+		}
+		else if(str[i] != '0' || str[i] == '0' && showZero == true){
+			cout << str[i];
+			noneZero = true;
+		}
+	}
+	if (noneZero == false)
+		cout << "0";
+	return 0;
+}
