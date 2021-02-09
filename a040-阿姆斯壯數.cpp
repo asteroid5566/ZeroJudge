1e81f51a9dca1b1551635883857131e497418054
asteroid5566 <47234920+asteroid5566@users.noreply.github.com>
2021-02-09 14:27:45 +0800

    solution

---
+++ a040-阿姆斯壯數.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, m, current, temp, digit, sum, exist = 0;
+	cin >> n >> m;
+	
+	for (int i = n; i <= m; i++){
+		current = i;
+		digit = 0;
+		sum = 0;
+		
+		while (current > 0){
+			current /= 10;
+			digit++;
+		}
+		
+		current = i;
+		for (int j = 0; j < digit; j++){
+			temp = 1;
+			for (int k = 0; k < digit; k++){
+				temp *= current % 10;
+			}
+			sum += temp;
+			current /= 10;
+		}
+		
+		if (sum == i){
+			exist = 1;
+			cout << sum << " ";
+		}
+	}
+	
+	if (exist == 0){
+		cout << "none";
+	}
+	
+	return 0;
+}
