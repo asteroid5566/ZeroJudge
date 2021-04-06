b14c5884b65e93241e2d95ab66c0fbe856dd1ec6
asteroid5566 <47234920+asteroid5566@users.noreply.github.com>
2021-04-06 12:05:10 +0800

    solution

---
+++ a059-完全平方和.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int T, S = 1, i, a, b, sum;
+	cin >> T;
+	
+	while (T--) {
+		cin >> a >> b;
+		sum = 0;
+		for (i = sqrt(a) - 1; i <= sqrt(b) + 1; i++) {
+			if (i * i > b)
+				break;
+			if (sqrt(i * i) == i && (i * i >= a && i * i <= b)) {
+				sum += i * i;
+			}
+		}
+		cout << "Case " << S++ << ": " << sum << endl;
+	}
+	return 0;
+}
