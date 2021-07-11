87f1028a2ee29928238d93853a01dc616233588e
asteroid5566 <47234920+asteroid5566@users.noreply.github.com>
2021-07-11 23:28:16 +0800

    Create a003-兩光法師占卜術.cpp

---
+++ a003-兩光法師占卜術.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int m, d, s;
+	cin >> m >> d;
+	s = (m * 2 + d) % 3;
+
+	switch (s)
+	{
+		case 0:
+			cout << "´¶³q" << endl;
+			break;
+		case 1:
+			cout << "¦N" << endl;
+			break;
+		case 2:
+			cout << "¤j¦N" << endl;
+	}
+	return 0;
+}
