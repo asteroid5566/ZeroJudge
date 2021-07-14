c09031d9b1247f0821c5e8f14c4b593268915901
asteroid5566 <47234920+asteroid5566@users.noreply.github.com>
2021-07-14 23:22:27 +0800

    Create a004-文文的求婚.cpp

---
+++ a004-文文的求婚.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t;
+	while(cin >> t) {
+		if(t % 4 == 0 && (t % 100 != 0 || t % 400 == 0)) {
+			cout << "¶|¦~" << endl;
+		}
+		else
+			cout << "¥­¦~" << endl;
+	}
+	return 0;
+}
