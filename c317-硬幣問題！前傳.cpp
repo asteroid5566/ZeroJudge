127d6c6997bc1bea9ec6a9d0afdc5707c0885d4d
asteroid5566 <47234920+asteroid5566@users.noreply.github.com>
2021-08-10 09:41:26 +0800

    Create c317-硬幣問題！前傳.cpp

---
+++ c317-硬幣問題！前傳.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, a, b, x, min, i, j;
+	cin >> n;
+	
+	while (n--) {
+		min = 999999;
+		cin >> x >> a >> b;
+		for (i = 0; i <= x / a; i++) {
+			for (j = 0; j <= x / b; j++) {
+				if (i * a + j * b == x && i + j < min) {
+					min = i + j;
+				}
+			}
+		}
+		if (min == 999999)
+			cout << -1 << endl;
+		else
+			cout << min << endl;
+	}
+	return 0;
+}
