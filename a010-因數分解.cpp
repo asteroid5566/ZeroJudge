31c373abbb612b638abeafd6351d1f9160e0b9b9
asteroid5566 <47234920+asteroid5566@users.noreply.github.com>
2021-09-07 17:33:32 +0800

    Update a010-因數分解.cpp

--- a010-因數分解.cpp
+++ a010-因數分解.cpp
@@ -1,3 +1,4 @@
+// https://zerojudge.tw/ShowProblem?problemid=a010
 #include <iostream>
 using namespace std;
 
@@ -7,26 +8,26 @@ int main()
 	int i = 2, n, power;
 	cin >> n;
 	
-	while (n > 1){
-		if (n % i == 0){
+	while (n > 1) {
+		if (n % i == 0) {
 			n /= i;
 			power = 1;
 
-			while (n % i == 0){
+			while (n % i == 0) {
 				n /= i;
 				power++;
 			}
 			
-			if (power == 1){
-				if (first == true){
+			if (power == 1) {
+				if (first == true) {
 					cout << i;
 					first = false;
 				}
 				else
 					cout << " * " << i;
 			}
-			else{
-				if (first == true){
+			else {
+				if (first == true) {
 					cout << i << "^" << power;
 					first = false; 
 				}
