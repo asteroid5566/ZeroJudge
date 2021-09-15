683080067662333b3630d9d799bd597454bd49c7
asteroid5566 <47234920+asteroid5566@users.noreply.github.com>
2021-09-15 12:23:10 +0800

    Create c716-Johnny B. Goode.cpp

---
+++ c716-Johnny B. Goode.cpp
@@ -0,0 +1,11 @@
+// https://zerojudge.tw/ShowProblem?problemid=c716
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string s;
+	getline(cin, s);
+	cout << "Go, " << s << ", go go" << endl;
+	return 0;
+}
