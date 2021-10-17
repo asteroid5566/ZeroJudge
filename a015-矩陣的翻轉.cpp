e087e333bd1fb4c816dc9eadbcd1d6bdad2f8740
asteroid5566 <47234920+asteroid5566@users.noreply.github.com>
2021-10-17 08:44:34 +0800

    Update a015-矩陣的翻轉.cpp

--- a015-矩陣的翻轉.cpp
+++ a015-矩陣的翻轉.cpp
@@ -6,15 +6,15 @@ int main()
 {
 	int mat[100][100], row, col, i, j;
 	
-	while (cin >> row >> col){
-		for (i = 0; i < row; i++){
-			for (j = 0; j < col; j++){
+	while (cin >> row >> col) {
+		for (i = 0; i < row; i++) {
+			for (j = 0; j < col; j++) {
 				cin >> mat[i][j];
 			}
 		}
 		
-		for (i = 0; i < col; i++){
-			for (j = 0; j < row; j++){
+		for (i = 0; i < col; i++) {
+			for (j = 0; j < row; j++) {
 				cout << mat[j][i] << " ";
 			}
 			cout << endl;
