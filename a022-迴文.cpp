28224f2affd349f6b9f8245a21ec58c1881c682b
asteroid5566 <47234920+asteroid5566@users.noreply.github.com>
2021-10-18 23:13:50 +0800

    Update a022-迴文.cpp

--- a022-迴文.cpp
+++ a022-迴文.cpp
@@ -1,3 +1,4 @@
+// https://zerojudge.tw/ShowProblem?problemid=a022
 #include <iostream>
 using namespace std;
 
@@ -6,17 +7,16 @@ int main()
 	string str;
 	bool flag;
 	
-	while (cin >> str){
+	while (cin >> str) {
 		flag = true;
 		
-		for (int i = 0; i < str.length() / 2; i++){
-			if (str[i] != str[str.length() - 1 - i]){
+		for (int i = 0; i < str.length() / 2; i++) {
+			if (str[i] != str[str.length() - 1 - i]) {
 				flag = false;
 				break;
 			}	
 		}
 		flag ? cout << "yes" << endl : cout << "no" << endl;
-	}
-	
+	}	
 	return 0;
 }
