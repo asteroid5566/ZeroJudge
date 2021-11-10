12e8ec0593753765f590dc139717965b3dc99ebc
asteroid5566 <47234920+asteroid5566@users.noreply.github.com>
2021-11-10 23:08:42 +0800

    Update a042-平面圓形切割.cpp

--- a042-平面圓形切割.cpp
+++ a042-平面圓形切割.cpp
@@ -1,11 +1,11 @@
+// // https://zerojudge.tw/ShowProblem?problemid=a042
 #include <iostream>
 using namespace std;
 
 int main()
 {
 	int n;
-	while (cin >> n){
+	while (cin >> n)
 		cout << n * n - n + 2 << endl;
-	}
 	return 0;
 }
