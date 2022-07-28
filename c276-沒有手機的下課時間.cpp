f1cb677d307778c619f5ac541a36904b17ed1b58
asteroid5566 <47234920+asteroid5566@users.noreply.github.com>
2022-07-29 00:09:05 +0800

    Update c276-沒有手機的下課時間.cpp

--- c276-沒有手機的下課時間.cpp
+++ c276-沒有手機的下課時間.cpp
@@ -1,31 +1,23 @@
+// https://zerojudge.tw/ShowProblem?problemid=c276
 #include <iostream>
 using namespace std;
 
 int main()
 {
-	int temp, n, arr[4], guess[4], a, b, i, j;
-
-	cin >> temp;
-	for (i = 0; i < 4; i++) {
-		arr[3 - i] = temp % 10;
-		temp /= 10;
-	}
-
-	cin >> n;
+	int n, a, b, i, j;
+	string q, g;
+	cin >> q >> n;
+	
 	while (n--) {
 		a = b = 0;
-		cin >> temp;
-		for (i = 0; i < 4; i++) {
-			guess[3 - i] = temp % 10;
-			temp /= 10;
-		}
+		cin >> g;
 		
 		for (i = 0; i < 4; i++) {
-			if (guess[i] == arr[i])
+			if (q[i] == g[i])
 				a++;
 			else {
 				for (j = 0; j < 4; j++) {
-					if (guess[j] == arr[i])
+					if (g[j] == q[i])
 						b++;
 				}
 			}
