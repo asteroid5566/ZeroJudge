9e9b0b7aa8bd335c68e5003d5adcc3a4100c4aa3
asteroid5566 <47234920+asteroid5566@users.noreply.github.com>
2022-08-12 17:40:39 +0800

    Create d073-分組報告.cpp

---
+++ d073-分組報告.cpp
@@ -0,0 +1,12 @@
+// https://zerojudge.tw/ShowProblem?problemid=d073
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	
+	cout << (n - 1) / 3 + 1 << endl;
+	return 0;
+}
