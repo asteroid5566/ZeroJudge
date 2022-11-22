70951acddeca1db3a7b3fcab067ecf7ba47455cd
asteroid5566 <47234920+asteroid5566@users.noreply.github.com>
2022-11-22 20:40:18 +0800

    Create e447-queue 練習.cpp

---
+++ e447-queue 練習.cpp
@@ -0,0 +1,36 @@
+// https://zerojudge.tw/ShowProblem?problemid=e447 
+#include <iostream>
+#include <set>
+#include <map>
+using namespace std;
+
+int arr[1000001];
+
+int main()
+{
+	int n, head = 0, tail = 0, k, x;
+	cin >> n;
+	
+	while (n--) {
+		cin >> k;
+		
+		if (k == 1) {
+			cin >> x;
+			if (!head)
+				head++;
+			tail++;
+			arr[tail] = x;
+		}
+		else if (k == 2) {
+			if (!head || head > tail)
+				cout << "-1\n";
+			else
+				cout << arr[head] << "\n";
+		}
+		else {
+			if (head && head <= tail)
+				head++;
+		}
+	}
+	return 0;
+}
