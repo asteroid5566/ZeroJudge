1a6bfd0d46c5c4c74cda29935bad27f23d59ed56
asteroid5566 <47234920+asteroid5566@users.noreply.github.com>
2023-04-30 19:29:39 +0800

    Create c575-APCS 2017-0304-4基地台.cpp

---
+++ c575-APCS 2017-0304-4基地台.cpp
@@ -0,0 +1,43 @@
+// https://zerojudge.tw/ShowProblem?problemid=c575
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int count, n, k, p[50000];
+
+bool test(int range)
+{
+	int i, curr = p[0] + range, count = 1;
+	
+	for (i = 1; i < n; i++) {
+		if (p[i] <= curr)
+			continue;
+		
+		curr = p[i] + range;
+		if (++count > k)
+			return false;
+	}
+	return true;
+}
+
+int main()
+{
+	int i, L = 1, R, M;
+	cin >> n >> k;
+	
+	for (i = 0; i < n; i++)
+		cin >> p[i];
+	sort(p, p + n);
+	
+	R = p[n - 1] - p[0];
+	
+	while (L != R) {
+		M = (L + R) / 2;
+		if (test(M))
+			R = M;
+		else
+			L = M + 1;
+	}
+	cout << L;
+	return 0;
+}
