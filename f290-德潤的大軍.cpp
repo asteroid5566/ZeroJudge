dbb36c45abd928e2342f8dddeac009feb9748959
asteroid5566 <47234920+asteroid5566@users.noreply.github.com>
2023-11-23 20:46:53 +0800

    Update f290-德潤的大軍.cpp

--- f290-德潤的大軍.cpp
+++ f290-德潤的大軍.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 struct VAL {
 	int atk, def;
-};
+} arr[1000000];
 
 bool cmp(VAL a, VAL b)
 {
@@ -16,7 +16,6 @@ int main()
 {
 	int n, i;
 	cin >> n;
-	VAL arr[n];
 	
 	for (i = 0; i < n; i++)
 		scanf("%d", &arr[i].def);
