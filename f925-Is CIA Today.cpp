ef527a30be0852227669c8120bdd15274e8f910b
asteroid5566 <47234920+asteroid5566@users.noreply.github.com>
2023-12-16 22:05:45 +0800

    Create f925-Is CIA Today.cpp

---
+++ f925-Is CIA Today.cpp
@@ -0,0 +1,15 @@
+// https://zerojudge.tw/ShowProblem?problemid=f925
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int m, d;
+	cin >> m >> d;
+	
+	if (m == 6 && d == 12)
+		cout << "Yes\n";
+	else
+		cout << "No\n";
+	return 0;
+}
