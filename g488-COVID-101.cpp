0e36ba224c0288d51af21e02ebb05b770692800d
asteroid5566 <47234920+asteroid5566@users.noreply.github.com>
2024-03-02 10:20:13 +0800

    Create g488-COVID-101.cpp

---
+++ g488-COVID-101.cpp
@@ -0,0 +1,16 @@
+// https://zerojudge.tw/ShowProblem?problemid=g488
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int arr[201], x, i;
+	cin >> x;
+	arr[1] = 1;
+	
+	for (i = 2; i < 201; i++)
+		arr[i] = arr[i - 1] + i * i - i + 1;
+	
+	cout << arr[x] << endl;
+	return 0;
+}
